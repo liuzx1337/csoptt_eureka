60339dbeb17514f1406a6da603b2ae5486388516
刘子西 <Liuzx1337@163.com>
2018-11-14 09:28:17 +0800

    更新配置

--- csoptt_eureka-main/src/main/java/com/csoptt/CsopttEurekaMainApplication.java
+++ csoptt_eureka-main/src/main/java/com/csoptt/CsopttEurekaMainApplication.java
@@ -1,8 +1,13 @@
 package com.csoptt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import javax.annotation.PostConstruct;
 
 /**
  * 启动类
@@ -12,7 +17,37 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class CsopttEurekaMainApplication {
 
+	/**
+	 * Log4j
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(CsopttEurekaMainApplication.class);
+
+	/**
+	 * 配置中心拉取的properties文件中的config.name属性的值
+	 */
+	@Value("${config.name}")
+	private String configName;
+
+	/**
+	 * 初始化
+	 */
+	private static CsopttEurekaMainApplication application;
+
+	/**
+	 * 初始化
+	 */
+	@PostConstruct
+	private void init() {
+		application = this;
+	}
+
 	public static void main(String[] args) {
+		// 显示拉取的配置文件的配置名称
+		try {
+			LOGGER.info(application.configName);
+		} catch (Exception e) {
+			LOGGER.error("拉取配置文件失败", e);
+		}
 		SpringApplication.run(CsopttEurekaMainApplication.class, args);
 	}
 }
